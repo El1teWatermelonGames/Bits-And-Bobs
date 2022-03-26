79f8f23623cd2d67e026fc722f4e0267ed7125dd
El1teWatermelonGames <EliteWatermelonGames@gmail.com>
2022-03-26 18:37:33 +0000

    New features
    - Sapphire Block added

--- src/main/java/com/EliteWatermelon/bitsandbobs/bitsandbobs.java
+++ src/main/java/com/EliteWatermelon/bitsandbobs/bitsandbobs.java
@@ -1,5 +1,6 @@
 package com.EliteWatermelon.bitsandbobs;
 
+import block.ModBlocks;
 import com.mojang.logging.LogUtils;
 import item.ModItems;
 import net.minecraft.world.level.block.Block;
@@ -34,6 +35,7 @@ public class bitsandbobs
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
