4a4cfb0ec8edd1bfe500cc5fbbbc084fba0126aa
El1teWatermelonGames <EliteWatermelonGames@gmail.com>
2022-03-26 17:41:48 +0000

    New features
    - Sapphires added
    
    New Backend
    
    -Parchment addded

---
+++ src/main/java/item/ModItems.java
@@ -0,0 +1,23 @@
+package item;
+
+import com.EliteWatermelon.bitsandbobs.bitsandbobs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, bitsandbobs.MOD_ID);
+
+    public static final RegistryObject<Item> Sapphire = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
